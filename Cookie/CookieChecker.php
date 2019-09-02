2a43c59bb5bf4773d24c9be7c0118945a4d528ae
xorgxx <zorg@pavl-in.com>
2019-09-02 15:39:37 +0200

    Update CookieChecker.php

--- Cookie/CookieChecker.php
+++ Cookie/CookieChecker.php
@@ -19,18 +19,48 @@ class CookieChecker
      * @var Request
      */
     private $request;
+    /**
+     * @var string
+     */
+    private $cookiePrefix;
 
-    public function __construct(Request $request)
+    /**
+     * @var string
+     */
+    private $cookieKeyName;
+
+    /**
+     * @var string
+     */
+    private $cookieConsentName;
+
+    /**
+     * CookieChecker constructor.
+     *
+     * @param Request $request
+
+     */
+    public function __construct(Request $request, string $cookiePrefix, string $cookieKeyName, string $cookieConsentName)
     {
-        $this->request = $request;
+        $this->request              = $request;
+        $this->cookiePrefix         = $cookiePrefix;
+        $this->cookieKeyName        = $cookieKeyName;
+        $this->cookieConsentName    = $cookieConsentName;
     }
 
     /**
      * Check if cookie consent has already been saved.
      */
-    public function isCookieConsentSavedByUser(): bool
+//    public function isCookieConsentSavedByUser(  ): bool
+//    {
+//        return $this->request->cookies->has(CookieNameEnum::COOKIE_CONSENT_NAME);
+//    }
+    /**
+     * @return bool
+     */
+    public function isCookieConsentSavedByUser(  ): bool
     {
-        return $this->request->cookies->has(CookieNameEnum::COOKIE_CONSENT_NAME);
+        return $this->request->cookies->has($this->cookieConsentName);
     }
 
     /**
@@ -38,6 +68,6 @@ class CookieChecker
      */
     public function isCategoryAllowedByUser(string $category): bool
     {
-        return $this->request->cookies->get(CookieNameEnum::getCookieCategoryName($category)) === 'true';
+        return $this->request->cookies->get(CookieNameEnum::getCookieCategoryName($category, $this->cookieConsentName)) === 'true';
     }
 }
