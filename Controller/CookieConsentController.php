5a88f1b3c2976da49620f67d1284c81bd07e55b3
Niklas <niklas.brunberg@ateles.se>
2021-06-04 14:13:07 +0200

    MaxAge = 0

--- Controller/CookieConsentController.php
+++ Controller/CookieConsentController.php
@@ -94,6 +94,7 @@ class CookieConsentController
 
         // Cache in ESI should not be shared
         $response->setPrivate();
+        $response->setMaxAge(0);
 
         return $response;
     }
